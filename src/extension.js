ebd49521164079419ea6cf5a07746c84ce9725f3
Jorge Alejandro Jimenez Luna <jorgeajimenezl17@gmail.com>
2023-11-03 16:14:39 -0400

    Rewrited nauta session class in order to make async

--- src/extension.js
+++ src/extension.js
@@ -12,12 +12,12 @@ import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 import * as QuickSettings from 'resource:///org/gnome/shell/ui/quickSettings.js';
 
 import * as NautaSession from './nautaSession.js';
-import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
+import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
 
-function formatTimeString(seconds) {
-    return "   %02d:%02d:%02d".format(
-        seconds / 3600, 
-        (seconds / 60) % 60, 
+function format_time_string(seconds) {
+    return "%02d:%02d:%02d".format(
+        seconds / 3600,
+        (seconds / 60) % 60,
         seconds % 60
     );
 }
@@ -28,10 +28,10 @@ class UserMenuItem {
         this.item = new PopupMenu.PopupMenuItem(user.username);
         this.session = session;
         this.settings = settings;
-        
+
         this.settings.bind(
-            "session-connected", 
-            this.item, "visible", 
+            "session-connected",
+            this.item, "visible",
             Gio.SettingsBindFlags.GET | Gio.SettingsBindFlags.INVERT_BOOLEAN
         );
 
@@ -40,20 +40,15 @@ class UserMenuItem {
         });
     }
 
-    login() {
-        this.session.login_async(
-            this.user.username,
-            this.user.password,
-            null,
-            (_, r) => {
-                if (r.had_error() || !this.session.login_finish(r)) {
-                    Main.notify(_("Unable to login right now"));
-                    return;
-                } else {
-                    Main.notify(`Logged with ${this.user.username}`);
-                }
-            },
-        );
+    async login() {
+        try {
+            await this.session.login(this.user.username, this.user.password);
+            this.session.save(this.settings);
+            Main.notify(`Logged with ${this.user.username}`);
+        } catch (e) {
+            Main.notify(_("Unable to login right now"));
+            console.error(e);
+        }
     }
 
     destroy() {
@@ -77,7 +72,7 @@ const NautaMenuToggle = GObject.registerClass(
             this._changedUserConnectionId = null;
             this._clickedConnectionId = null;
             this.items = [];
-            
+
             const icon = Gio.icon_new_for_string(extensionObject.path + '/icons/etecsa-logo.svg');
 
             super._init({
@@ -94,7 +89,7 @@ const NautaMenuToggle = GObject.registerClass(
 
             // Setup logout action
             this._clickedConnectionId = this.connect(
-                "clicked", () => { 
+                "clicked", () => {
                     const username = this.settings.get_string("current-username");
                     const connected = this.settings.get_boolean("session-connected");
 
@@ -102,23 +97,26 @@ const NautaMenuToggle = GObject.registerClass(
                         for (const item of this.items) {
                             if (item.user.username == username) {
                                 console.log(`Trying to login with: ${username}`);
-                                item.login();                                
+                                item.login();
                             }
                         }
                     } else {
                         // Logout   
-                        console.log(`Trying to logout with: ${username}`);    
-                        this.session.logout_async(null, (_, r) => {
-                            if (r.had_error() || !this.session.logout_finish(r)) {
-                                Main.notify(_("Unable to logout from actual session"));
-                            } else {
+                        console.log(`Trying to logout with: ${username}`);
+                        this.session.logout().then(
+                            () => {
                                 Main.notify(_("Session closed successfully"));
+                                this.session.save(this.settings);
+                            },
+                            (e) => {
+                                console.error(e);
+                                Main.notify(_("Unable to logout from actual session"));
                             }
-                        });
+                        );
                     }
                 },
-            );           
-            
+            );
+
             // Connect settings
             this._changedUserConnectionId = this.settings.connect(
                 "changed::current-username", () => {
@@ -176,11 +174,11 @@ const NautaMenuToggle = GObject.registerClass(
                     item.item.setOrnament(
                         (username == user) ? PopupMenu.Ornament.DOT
                             : PopupMenu.Ornament.NONE);
-                    
+
                     this.items.push(item);
                     this.menu.addMenuItem(item.item);
-                }      
-                
+                }
+
                 // Setup setting
                 // FIX: Need to push this code here coz if
                 // not, the order is random (async)
@@ -188,26 +186,26 @@ const NautaMenuToggle = GObject.registerClass(
                 const settingsItem = this.menu.addAction("Settings", () => this._extensionObject.openPreferences());
                 settingsItem.visible = Main.sessionMode.allowSettings;
                 this.menu._settingsActions[this._extensionObject.uuid] = settingsItem;
-            });             
+            });
         }
     }
 );
 
 const NautaIndicator = GObject.registerClass(
-    class NautaIndicator extends PanelMenu.Button { 
+    class NautaIndicator extends PanelMenu.Button {
         _init(extensionObject) {
             super._init({
                 reactive: true,
                 can_focus: true,
                 track_hover: true,
                 accessible_role: Atk.Role.PUSH_BUTTON,
-            });                
+            });
             this.add_style_class_name("nauta-indicator");
 
             this.settings = extensionObject.getSettings();
-            this.session = new NautaSession.NautaSession(this.settings);
+            this.session = new NautaSession.NautaSession();
             this.quickSettingsItems = [];
-            
+
             this._extensionObject = extensionObject;
             this._refreshTimerConnectionId = null;
             this._connectedConnectionId = null;
@@ -215,15 +213,15 @@ const NautaIndicator = GObject.registerClass(
             this._totalTime = null;
             this._notified = false;
 
-            this.buildUI();           
-                               
+            this.buildUI();
+
             this._connectedConnectionId = this.settings.connect(
                 "changed::session-connected", () => {
                     const connected = this.settings.get_boolean("session-connected");
 
                     if (connected) {
                         this.show();
-                        this.setupTimer();                        
+                        this.setupTimer();
                     } else {
                         this.hide();
                         this._totalTime = null;
@@ -231,11 +229,11 @@ const NautaIndicator = GObject.registerClass(
                     }
                 },
             );
-            
+
             // Create the toggle menu and associate it with the indicator, being
             // sure to destroy it along with the indicator
             this.quickSettingsItems.push(new NautaMenuToggle(extensionObject, this.session));
-            
+
             // Add the indicator to the panel and the toggle to the menu
             Main.panel.addToStatusArea(extensionObject.uuid, this);
             Main.panel.statusArea.quickSettings.addExternalIndicator(this);
@@ -250,12 +248,10 @@ const NautaIndicator = GObject.registerClass(
                 y_align: Clutter.ActorAlign.CENTER,
                 visible: true,
             });
-            
-            this._box.add_child(this._label);
 
+            this._box.add_child(this._label);
             this._icon = new St.Icon({ icon_name: "window-close-symbolic" });
             this._box.add_child(this._icon);
-
             const connected = this.settings.get_boolean("session-connected");
 
             if (connected) {
@@ -268,37 +264,37 @@ const NautaIndicator = GObject.registerClass(
         vfunc_event(event) {
             if (event.type() === Clutter.EventType.TOUCH_BEGIN ||
                 event.type() === Clutter.EventType.BUTTON_PRESS) {
-                    // Logout
-                    console.log(`Trying to logout with: ${username}`);
-                    this.session.logout_async(null, (_, r) => {
-                        if (r.had_error() || !this.session.logout_finish(r)) {
-                            Main.notify(_("Unable to logout from actual session"));
-                        } else {
-                            Main.notify(_("Session closed successfully"));
-                        }
-                    });
-                }
-    
+                console.log(`Trying to logout with: ${username}`);
+                this.session.logout().then(
+                    () => {
+                        Main.notify(_("Session closed successfully"));
+                        this.session.save(this.settings);
+                    },
+                    (e) => {
+                        console.error(e);
+                        Main.notify(_("Unable to logout from actual session"));
+                    }
+                );
+            }
+
             return Clutter.EVENT_PROPAGATE;
-        }    
+        }
 
-        setupTimer() {
-            this._startTime = GLib.get_monotonic_time();                         
+        async setupTimer() {
+            this._startTime = GLib.get_monotonic_time();
 
-            // get remained time
-            this.session.get_remaining_time_async(null, (_, r) => {
-                try {
-                    this._totalTime = this.session.get_remaining_time_finish(r);
-                } catch (e) {
-                    console.warn("Unable to get the remaining time in this session");
-                    this._totalTime = null;
-                }
-            });
+            try {
+                this._totalTime = await this.session.remaining_time();
+            } catch (e) {
+                console.warn("Unable to get the remaining time in this session");
+                this._totalTime = null;
+            }
 
             const info = this.settings.get_string("time-info");
             if (info != "none") {
                 // timer update
-                this._refreshTimerConnectionId = GLib.timeout_add_seconds(1.0, (self) => {
+                this._refreshTimerConnectionId = GLib.timeout_add_seconds(
+                    GLib.PRIORITY_DEFAULT, 1.0, () => {
                     this.updateTimer();
                     return true;
                 });
@@ -318,12 +314,12 @@ const NautaIndicator = GObject.registerClass(
             const info = this.settings.get_string("time-info");
 
             if (info == "remain" && this._totalTime == null) {
-                this._label.text = "   No Available :(";
+                this._label.text = "No Available :(";
             } else {
                 if (info == "remain")
                     seconds = this._totalTime - seconds;
 
-                this._label.text = formatTimeString(seconds);
+                this._label.text = format_time_string(seconds);
             }
         }
 
